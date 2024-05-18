9d9b30589aa7de7a17a360bf0e8e81d20e664753
hafijure <hafijure@gmail.com>
2024-05-18 13:13:57 +0600

    login with github complete

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import app from '../../firebase/firebase.init';
-import { GoogleAuthProvider, getAuth, signInWithPopup,  } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut,  } from 'firebase/auth';
 
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app)
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const gitHubProvider = new GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
         .then((result) => {
             // This gives you a Google Access Token. You can use it to access the Google API.
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
             // The signed-in user info.
-            const user = result.user;
+            const authenticUser = result.user;
+            setUser(authenticUser);
             // IdP data available using getAdditionalUserInfo(result)
-            console.log(user);
+            console.log(authenticUser);
             // ...
           }).catch((error) => {
             // Handle Errors here.
@@ -29,9 +32,55 @@ const Login = () => {
           });
     }
 
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth, gitHubProvider)
+  .then((result) => {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    const credential = GithubAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const authenticUser = result.user;
+    setUser(authenticUser);
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GithubAuthProvider.credentialFromError(error);
+    // ...
+  });
+    }
+
+    const handleSignOut = () => {
+        signOut(auth)
+        .then((reuslt) => {
+            setUser(null);
+            console.log(reuslt);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Google login</button>
+        {user ? <button onClick={handleSignOut}>Sign Out</button> :
+        <>
+        <button onClick={handleGoogleSignIn}>Google login</button>
+        <button onClick={handleGithubSignIn}>GitHub login</button>
+        </>
+        }
+            { user && <div>
+                <h2>Username: {user.displayName}</h2>
+                <p>Email: {user.email}</p>
+                <p>UId: {user.uid}</p>
+                <img src={user.photoURL} alt="" />
+            </div> }
         </div>
     );
 };
